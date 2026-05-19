c24449996a6cced7ced11b2ca50da85d4484bb7f
josselin29 <j.brassaud@outlook.com>
2026-05-19 14:15:50 +0200

    Update functions.php

--- intranet/includes/functions.php
+++ intranet/includes/functions.php
@@ -18,3 +18,26 @@ function sauvegarderJson($nomDuFichier, $tableauDonnees) {
     $texteJson = json_encode($tableauDonnees, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE); //tbs en txt
     file_put_contents($chemin, $texteJson);
 }
+
+function afficherHeaderEtNavbar() {
+    ?>
+    <!DOCTYPE html>
+    <html lang="fr">
+    <head>
+        <title>Intranet</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    </head>
+    <body class="bg-light">
+
+        <nav class="navbar navbar-dark bg-primary shadow-sm mb-4">
+            <div class="container-fluid">
+                <span class="navbar-brand fw-bold">Intranet </span>
+                <div>
+                    <a href="annuaire.php" class="btn btn-sm btn-outline-light me-2">Annuaire</a>
+                    <a href="fichiers.php" class="btn btn-sm btn-outline-light me-2">Documents</a>
+                    <a href="../logout.php" class="btn btn-sm btn-danger">Déconnexion</a>
+                </div>
+            </div>
+        </nav>
+    <?php
+}
