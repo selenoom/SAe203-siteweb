5dbd90db04ff1250ac992f4e9c6e3857300b2e8e
josselin29 <j.brassaud@outlook.com>
2026-06-08 16:44:11 +0200

    Update annuaire.php

--- intranet/pages/annuaire.php
+++ intranet/pages/annuaire.php
@@ -6,10 +6,12 @@ $employes = lireJson('employees.json');
 <!DOCTYPE html>
 <html lang="fr">
 <head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <meta charset="UTF-8">
     <title>L'annuaire</title>
 </head>
 <body>
+    <?php afficherHeaderEtNavbar(); ?>
 
     <h1>La liste du Personnel/employés</h1>
 
@@ -23,7 +25,7 @@ $employes = lireJson('employees.json');
             </li>
         <?php 
         } 
-        ?> //ici boucle pour chaque employé
+        ?> 
     </ul>
 
 </body>
